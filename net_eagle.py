27e6084fc101342e61a3ed7b931992d9ec435da6
Ajmal Nasumudeen <64580276+ajmalnasumudeen@users.noreply.github.com>
2021-01-06 20:06:28 +0530

    initial file
    code file uploaded

---
+++ net_eagle.py
@@ -0,0 +1,19 @@
+import scapy.all as scapy
+def scan(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast.arp_request
+    answer_list = scapy.srp(arp_request_broadcast, timeout=2, verbose=false)[0]
+
+    client_list = []
+    for packet in answer_list:
+        client_dt = {"ip": packet[1].psrc, "mac": packet[1].hwsrc}
+        client_list.append(client_dt)
+        return client_list
+def print_result(result):
+    print("IP\t\t\tMAC Address\n-----------------------")
+    for users in result_list:
+        print(users["ip"] + "\t\t" + users["mac"])
+
+scan_result = scan("10.0.2.1/24")
+print_result(scan_result)
---
+++ readme.txt
@@ -0,0 +1,8 @@
+######### Net Eagle ########
+#!Python based project
+
+1) Make sure python 2.7 or greater installed
+2) start program using py mac-changer.py
+3) Select the network interface 
+ 
+##################################################
